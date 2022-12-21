a32a67b09f9d3ffa73cdf250484a4f5b7bc7b257
valla <valla@vallaui-MacBookPro.local>
2022-12-21 10:33:18 +0900

    button 기능 및 disabled ui까지

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('the counter starts at 0', () => {
@@ -6,3 +6,38 @@ test('the counter starts at 0', () => {
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
 });
+test('minus button has correct text', () => { 
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+  expect(buttonElement).toHaveTextContent("-");
+
+ })
+
+ test('plus button has correct text', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  expect(buttonElement).toHaveTextContent("+");
+ })
+
+ test('when the + button is pressed, the counter changes to 1', () => { 
+  render(<App />);
+  const buttonElement = screen.getByTestId("plus-button");
+  fireEvent.click(buttonElement);
+  const counterElement= screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+  })
+
+  test('on/off button has blue collor', () => { 
+    render(<App />);
+    const buttonElement = screen.getByTestId("on/off-button");
+    expect(buttonElement).toHaveStyle({ backgroundColor : "blue"});
+   });
+
+   test('Prevent the -,+ button from being pressed when the on/off button is clicked', () => { 
+    render(<App />);
+    const onOffButtonElement = screen.getByTestId("on/off-button");
+    fireEvent.click(onOffButtonElement);
+    const plusButtonElement = screen.getByTestId("plus-button");
+    expect(plusButtonElement).toBeDisabled();
+    })
